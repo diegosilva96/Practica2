87fda4df8a3a90c626c1cbd061d062f8a1f7ddf2
Tito <titoyanac@gmail.com>
2021-12-21 18:48:36 -0500

    Delete src/main/java/com/mycompany directory

--- src/main/java/com/mycompany/interfaces/impl/Interfaces/IEstadoEncuesta.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.interfaces.Interfaces;
-
-import com.mycompany.interfaces.impl.Encuesta;
-
-public interface IEstadoEncuesta {
-    void procesar(Encuesta encuesta);
-    void setEncuesta(Encuesta encuesta);
-}
