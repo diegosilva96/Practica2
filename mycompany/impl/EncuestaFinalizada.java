34250ce237905164f3e2032768f8fbb3e27f976a
diegosilva96 <92829873+diegosilva96@users.noreply.github.com>
2021-12-21 19:11:14 -0500

    Update EncuestaFinalizada.java

--- mycompany/impl/EncuestaFinalizada.java
+++ mycompany/impl/EncuestaFinalizada.java
@@ -8,6 +8,15 @@ public class EncuestaFinalizada implements IEstadoEncuesta {
     private Encuesta Encuesta;
     @Override
     public void procesar(Encuesta e) {   
+      IRespuestaIterator iterator = e.iterator();
+        while (iterator.hasNext()) {
+            String pregunta = iterator.next();
+            System.out.println("Pregunta: " + pregunta.listarPregunta());
+
+           for(String  respuesta: pregunta.getRespuestas() ){
+                System.out.println("Respuesta:" + respuesta);
+            }
+        }
         System.out.println("Encuesta finalizada...");
     }
 
